3d863bc94f5c6ab458f6a8d3b478ba9fd3e7571e
maistewart <maistewarte@gmail.com>
2014-07-02 17:28:28 -0400

    made external js file

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 /**
  * Created by Mai on 7/1/2014.
  */
+document.getElementById("step1").addEventListener("mouseout",nextfunc);
+
+function nextfunc(){
+    document.getElementById("plan").style.opacity = 0;
+}
+document.getElementById("step1").addEventListener("mouseover",myfunc);
+
+function myfunc(){
+    document.getElementById("plan").style.opacity = 0.7;
+}
